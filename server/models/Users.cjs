560d65fb9bb572356ec8e3123db377d5499a20d2
Tim Engle <timothy.englew@gmail.com>
2023-07-25 12:04:08 -0700

    🔀Update createUser

--- server/models/Users.cjs
+++ server/models/Users.cjs
@@ -3,6 +3,7 @@ const connectDB = require('../db/index.cjs');
 let usersCollection;
 let schedulesCollection;
 let contactsCollection;
+let medicineCollection;
 
 const connectToCollection = async () => {
   if (usersCollection) {
@@ -13,6 +14,7 @@ const connectToCollection = async () => {
     usersCollection = db.collection('users');
     schedulesCollection = db.collection('schedules');
     contactsCollection = db.collection('contacts');
+    medicineCollection = db.collection('medicines');
   } catch (err) {
     console.error(err);
     throw err;
@@ -30,31 +32,63 @@ module.exports = {
       email: user.email,
       authenticated: true,
       authorization: 'user',
-      qrCode: null,
+      qrCode:
+        'https://image-charts.com/chart?chs=150x150&cht=qr&chl=http://127.0.0.1:5173&choe=UTF-8',
       allergies: user.allergies,
     };
 
-    const scheduleDocument = sched.map((medicine) => ({
+    const medicineDocuments = sched.map((medicine) => ({
       name: medicine.name,
       time: medicine.time,
       timeTaken: null,
       taken: false,
     }));
 
+    //  Insert user
     const userResult = await usersCollection.insertOne({ ...userDocument });
     const userId = userResult.insertedId;
-    const schedResult = await schedulesCollection.insertOne({
-      userId,
-      scheduleDocument,
-    });
+
+    //  Insert Medicine
+    await Promise.all(
+      medicineDocuments.map(
+        async (med) => await medicineCollection.insertOne({ userId, ...med })
+      )
+    );
+
+    // Insert Contacts
     const contactResult = await contactsCollection.insertOne({
       contacts,
       userId,
     });
 
+    // AggregateMedicine
+    const medsCursor = await medicineCollection.aggregate([
+      {
+        $match: {
+          userId: userId,
+        },
+      },
+      {
+        $group: {
+          _id: '$userId',
+          medicines: {
+            $push: '$$ROOT',
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+    ]);
+
+    const medsData = await medsCursor.toArray();
+
     const responseBody = {
       user: { id: userId, ...userDocument },
-      schedule: { id: schedResult.insertedId, scheduleDocument },
+      // schedule: { id: schedResult.insertedId, scheduleDocument },
+      medicines: medsData[0].medicines,
       contacts: { id: contactResult.insertedId, contacts },
     };
     return responseBody;
