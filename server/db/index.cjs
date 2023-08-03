aa9786482720163c23603a57cce9c047c6b8b296
Tim Engle <timothy.englew@gmail.com>
2023-08-02 23:04:18 -0700

    📦 Update DB variables

--- server/db/index.cjs
+++ server/db/index.cjs
@@ -1,14 +1,14 @@
 const { MongoClient } = require('mongodb');
 
 const NAME = process.env.DB_NAME;
-// const USERNAME = process.env.DB_USER;
-// const PASS = process.env.DB_PASS;
-// const ADDRESS = process.env.DB_LOCATION;
-// const PORT = process.env.DB_PORT;
-// const AUTH = process.env.DB_AUTH;
+const USERNAME = process.env.DB_USER;
+const PASS = process.env.DB_PASS;
+const ADDRESS = process.env.DB_LOCATION;
+const PORT = process.env.DB_PORT;
+const AUTH = process.env.DB_AUTH;
 
-// const uri = `mongodb//${USERNAME}:${PASS}@${ADDRESS}:${PORT}/?authMechanism=DEFAULT&authSource=${AUTH}`;
-const uri = 'mongodb://localhost:27017/';
+const uri = `mongodb//${USERNAME}:${PASS}@${ADDRESS}:${PORT}/?authMechanism=DEFAULT&authSource=${AUTH}`;
+// const uri = 'mongodb://localhost:27017/';
 const client = new MongoClient(uri);
 let db;
 
